ae1e7f3434917f9a10fba6e4b250222039b1df8b
Tatiana <eremkinatatiana5@gmail.com>
2021-03-10 14:48:20 +0300

    добавлены функции, ассоциированные с объектами

--- sandbox/src/main/java/ru/stqa/pft/sandbox/myFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/myFirstProgram.java
@@ -8,26 +8,17 @@ public class myFirstProgram {
 		hello("user");
 		hello("Tatiana");
 
-		double len = 5;
-		System.out.println("Площаь квадрата со стороной " + len + " = " + area(len));
+		Square s = new Square(5);
+		System.out.println("Площаь квадрата со стороной " + s.l + " = " + s.area());
 
-		double a = 4;
-		double b = 6;
-		System.out.println("Площаь прямоугольника со сторонам " + a + " и "+ b + " = " + area(a, b));
+		Rectangle r = new Rectangle(4, 6);
+		System.out.println("Площаь прямоугольника со сторонам " + r.a + " и "+ r.b + " = " + r.area());
 
 	}
 
 	public static void hello(String somebody){
 
 		System.out.println("Hello " + somebody + "!");
-	}
-
-	public static double area (double l){
-		return l * l;
-	}
-
-	public static double area(double a, double b){
-		return a * b;
 	}
 
 }
